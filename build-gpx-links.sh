7d75e488fba5bd40b74f97fab87130d600741d18
L. David Baron <dbaron@dbaron.org>
2020-05-02 21:11:40 -0700

    Support more than one walk per day (without distinguishing them in the UI).

--- build-gpx-links.sh
+++ build-gpx-links.sh
@@ -6,9 +6,19 @@ rm -rf gpx-files
 mkdir gpx-files
 cd gpx-files
 
+PREVDATE=
+PREVCOUNT=
 ls ../../../../images/digital-camera/*Palo-Alto*/*Walk*.gpx | grep -v Baylands | while read GPXFILE
 do
     DATE=$(echo "$GPXFILE" | cut -d/ -f7 | cut -b1-10)
+    if [ "$PREVDATE" = "$DATE" ]
+    then
+        PREVCOUNT="$(($PREVCOUNT + 1))"
+        DATE="${DATE}-${PREVCOUNT}"
+    else
+        PREVDATE="$DATE"
+        PREVCOUNT=1
+    fi
     ln -s "$GPXFILE" "$DATE.gpx"
     echo "$DATE.gpx" >> list.txt
 done
